1aed93dbe56ba57607b459bf627709ad7823307c
Msgl <mariasglynou@gmail.com>
2023-01-31 15:24:33 +0200

    Added filter functionality in projects

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,18 @@ function toggleIcon(icon) {
   icon.classList.toggle('fa-bars');
   icon.classList.toggle('fa-times');
 }
+
+$(document).ready(function(){
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all"){
+          $('.filter').show('1100');
+      }
+      else{
+          $(".filter").not('.'+value).hide('2800');
+          $('.filter').filter('.'+value).show('2800');
+          
+      }
+  });
+});
